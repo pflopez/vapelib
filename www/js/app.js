e0b948cc36d24a233669ee11fb5aa8831dd21e68
Pablo Lopez <pflopez@gmail.com>
2015-01-31 20:13:56 -0500

    added proto form with local storage

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services' , 'ionic.utils'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -38,53 +38,53 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.vapes', {
+    url: '/vapes',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
+      'tab-vapes': {
+        templateUrl: 'templates/tab-vapes.html',
         controller: 'DashCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+  // .state('tab.chats', {
+  //     url: '/chats',
+  //     views: {
+  //       'tab-chats': {
+  //         templateUrl: 'templates/tab-chats.html',
+  //         controller: 'ChatsCtrl'
+  //       }
+  //     }
+  //   })
+  //   .state('tab.chat-detail', {
+  //     url: '/chats/:chatId',
+  //     views: {
+  //       'tab-chats': {
+  //         templateUrl: 'templates/chat-detail.html',
+  //         controller: 'ChatDetailCtrl'
+  //       }
+  //     }
+  //   })
 
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
+  // .state('tab.friends', {
+  //     url: '/friends',
+  //     views: {
+  //       'tab-friends': {
+  //         templateUrl: 'templates/tab-friends.html',
+  //         controller: 'FriendsCtrl'
+  //       }
+  //     }
+  //   })
+  //   .state('tab.friend-detail', {
+  //     url: '/friend/:friendId',
+  //     views: {
+  //       'tab-friends': {
+  //         templateUrl: 'templates/friend-detail.html',
+  //         controller: 'FriendDetailCtrl'
+  //       }
+  //     }
+  //   })
 
   .state('tab.account', {
     url: '/account',
@@ -97,6 +97,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/vapes');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,80 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $localstorage) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+	var vapes, flavors;
+	
+	
+
+
+
+
+
+
+
+
+
+ 	function getLocalStorage(){
+ 		vapes = $localstorage.getObject('vapesInfos');
+ 		return vapes;
+ 	}
+
+ 	function getFlavorsFromLocalStorage(){
+ 		vapes = $localstorage.getObject('vapesInfos');
+ 		flavors = vapes.flavors
+ 		return flavors;
+ 	}
+
+
+ 	function resetLocalStorage(){
+ 		$localstorage.setObject('vapesInfos',  {});
+ 	}
+
+ 	function saveVape(vape){
+ 		if(!flavors) {
+ 			flavors = [];	
+ 		}
+ 		flavors.push(vape);
+ 		console.log(vape);
+ 		saveLocalStorage();
+ 		return flavors;
+ 	}
+
+ 	function saveLocalStorage(){
+ 		$localstorage.setObject('vapesInfos',  {
+			name: "vaperland",
+			flavors: flavors
+		});
+ 	}
+
+ 	function init(){
+		//resetLocalStorage();	
+		$scope.vapedata = {};
+ 		if(getFlavorsFromLocalStorage()){
+ 			console.log("tengo flavors");
+ 			console.log(flavors);
+ 		}else {
+ 			console.log('no tengo flavors');
+ 			saveVape({name: 'test'});
+ 		}
+
+ 		$scope.flavors = flavors;
+ 		 	
+ 	}
+
+ 	 $scope.addVape = function(){
+ 	 	var data  = angular.copy($scope.vapedata);
+	 	saveVape(data);
+ 		$scope.vapedata = {};
+	 }
+
+
+ 	init();
+
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
